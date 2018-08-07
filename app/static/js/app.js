ee1a054819be1a348b154eaebfebe679168250d9
james-chege <jymeschege@gmail.com>
2018-08-07 16:53:58 +0300

    Users can view the number of entries they have

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -275,9 +275,39 @@ document.addEventListener('DOMContentLoaded', () => {
 
         })})
             .catch(err => console.log(err));
+
+
 });
 
+/////////////////////////////////
+// Number of entries user Has  //
+/////////////////////////////////
 
+document.addEventListener('DOMContentLoaded', () => {
+    const token = JSON.parse(localStorage.getItem('access_token'));
+    // get number of entries
+    const EntriesUrl = '//api-thriller-diary.herokuapp.com/api/v1/entries/';
+    fetch(`${EntriesUrl}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "access_token": token
+        }
+    })
+
+    .then((response)=>{
+        response.json().then((data) => {
+            let entries_number = document.getElementById('num_of_entries');
+            const entries = Object.values(data['Entries'])
+            if (entries.length == 0){
+                entries_number.innerHTML = 0;
+            }
+            else{
+                entries_number.innerHTML = entries.length;
+            }
+        })})
+            .catch(err => console.log(err));
+});
 ///////////////////////////////////
 //  USER LOGOUT                 //
 //////////////////////////////////
