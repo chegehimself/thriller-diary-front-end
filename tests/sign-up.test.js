ea2aa71ceddf54db65d688fa9101b99af49dbaf2
james-chege <jymeschege@gmail.com>
2018-08-29 15:46:16 +0300

    Test signup with a correct Email

--- tests/sign-up.test.js
+++ tests/sign-up.test.js
@@ -92,4 +92,36 @@ it('User signs with correct username', async () => {
     expect(document.getElementById('return').innerHTML).toBe('<h5 class=\"text-red\">That email is already registered</h5>');
 })
     
+it('User signs with correct email', async () => {
+    // restore mock to initial state
+    Mock.mockRestore();
+    NavSpyMock.mockRestore();
+    jest.resetModules();
+
+    // mock fake fetch
+    Mock = jest.spyOn(global, 'fetch');
+    Mock.mockImplementation(() => Promise.resolve({
+    
+    json: () => Promise.resolve({"status": "fail", "Message": "Invalid email.Try again"})
+    }))
+    document.getElementById('email').value = 'thisIsnotanEmail';
+    document.getElementById('DoSubmission').click();
+    expect(Mock).toHaveBeenCalledTimes(3);
+    const Fetch = Mock.mock.calls[0];
+    expect(Fetch[0]).toBe('//api-thriller-diary.herokuapp.com/api/v1/auth/signup');
+    expect(Fetch[1]).toEqual({
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+          email: 'thisIsnotanEmail',
+          password: '1234',
+          username: '@@#BadVeryB@d',
+      })
+    });
+    // wait for  the promise to resolve
+    await Promise.resolve().then();
+    expect(document.getElementById('return').innerHTML).toBe('<h5 class=\"text-red\">Invalid email.Try again</h5>');
+})
 
