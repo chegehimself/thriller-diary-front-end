cc0908e0f43cb00341809fa8c2d19036c7042bd6
james-chege <jymeschege@gmail.com>
2018-08-30 16:05:12 +0300

    More Testing on entry Update

--- tests/modify.test.js
+++ tests/modify.test.js
@@ -20,7 +20,8 @@ document.body.innerHTML =`
 describe ('Successfully delete an entry from thriller-diary.herokuapp.com', () => {
     Mock = jest.spyOn(global, 'fetch');
     Mock.mockImplementation(() => Promise.resolve({
-    json: () => Promise.resolve({"status":"success", "Updated":"id"})
+    json: () => Promise.resolve({"status": "success",
+    "Updated":{"id":'id_entry',"title":'entries_user', "description": 'entries_user'}})
     }))
     // spy on window navigation
     NavSpyMock = jest.spyOn(window.location, "assign");
@@ -92,7 +93,10 @@ describe ('Successfully delete an entry from thriller-diary.herokuapp.com', () =
 
         Mock = jest.spyOn(global, 'fetch');
         Mock.mockImplementation(() => Promise.resolve({
-        json: () => Promise.resolve({"title":"Tittle", "description":"Description"})
+        json: () => Promise.resolve({"status": "success", "entry": {"id":1,
+        "title":"fakeTittle",
+        "description":"fakeDescription",
+        "created":"30/08/2018"}})
         }))
 
         // spy on window navigation
@@ -116,7 +120,43 @@ describe ('Successfully delete an entry from thriller-diary.herokuapp.com', () =
               }
             })
             await Promise.resolve().then();
-            expect(document.getElementById('title').value).toBe('newTittle');
-            expect(document.getElementById('description').value).toBe('newDescription');
+            expect(document.getElementById('title').value).toBe('fakeTittle');
+            expect(document.getElementById('description').value).toBe('fakeDescription');
+      });
+
+      it('updates a certain entry with a valid title at thriller-diary.herokuapp.com', async () => {
+        // restore mock to initial state
+        Mock.mockRestore();
+        NavSpyMock.mockRestore();
+        jest.resetModules();
+
+        // spy on window navigation
+        NavSpyMock = jest.spyOn(window.location, "assign");
+        NavSpyMock.mockImplementation(() => {});
+
+        Mock = jest.spyOn(global, 'fetch');
+        Mock.mockImplementation(() => Promise.resolve({
+        json: () => Promise.resolve({})
+        }))
+        window.history.pushState({}, 'delete', 'http://thriller-diary.herokuapp/entries/1');
+        entryId = Number(location.pathname.match(/\d+/)[0]);
+        document.getElementById('updater').click();
+        expect(Mock).toHaveBeenCalledTimes(3);
+        const Fetch = Mock.mock.calls[0];
+        expect(Fetch[0]).toBe('//api-thriller-diary.herokuapp.com/api/v1/entries/1');
+        expect(Fetch[1]).toEqual({
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            access_token : null
+          },
+          body: JSON.stringify({
+              title: "fakeTittle",
+              description: "fakeDescription",
+          }) 
+        });
+        // wait for  the promise to resolve
+        await Promise.resolve().then();
+        expect(document.getElementById('errors').innerHTML).toBe('<h3 class=\"text-blue\">Processing...</h3>');
       });
  });
