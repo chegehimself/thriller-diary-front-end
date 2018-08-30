33b4179d74e94eeed31a4c57ba8938128e404e96
james-chege <jymeschege@gmail.com>
2018-08-30 19:14:42 +0300

    [CH-TESTS] whether homepage is fetched correctly

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,47 +1,5 @@
 'use strict'
 
-/////////////////////////////////////////////////////
-//   HOMEPAGE WELCOME MESSAEGE                     //
-/////////////////////////////////////////////////////
-document.addEventListener('DOMContentLoaded', () => {
-    // fetch for landing page
-let message = document.getElementById('welcoming-message');
-
-// const WelcomeUrl = '//api-thriller-diary.herokuapp.com/api/v1/auth/';
-const WelcomeUrl = '//api-thriller-diary.herokuapp.com/api/v1/auth/';
-fetch(`${WelcomeUrl}`, {
-    method: "GET"
-})
-
-    .then((response)=>{
-        response.json().then((data) => {
-        console.log(data)
-        const warmWelcome = Object.values(data.Message)
-
-        
-        const FetchedMessage =  ` 
-                                <p class="text-white">
-                                    Are you tired of writing your dairy on paper and having to carry 
-                                    the book everywhere you go?
-                                </p>
-                                <p class="text-white">
-                                    Imagine of accessing your Dairy from everywhere you want, at anytime you want 
-                                    and suprisingly, not only with a computer but also a mobile device readily 
-                                    available from your pocket or handbag..Or just borrow from a friend for a second.
-                                </p>
-                                <p class="text-white">
-                                    Your Struggle Ends right now, right here! Click the button below to join us and get a chance to 
-                                    enjoy our <b>free</b> services.
-                                </p>
-                                `
-
-        message.innerHTML = FetchedMessage
-        
-        })})
-        .catch(err => console.log(err));
-    // your code here
- }, false);
-
 
 
 /////////////////////////////////////////////////////
---
+++ tests/welcome.test.js
@@ -0,0 +1,32 @@
+describe ('Successfully delete an entry from thriller-diary.herokuapp.com', () => {
+    document.body.innerHTML = `<div id="welcoming-message"></div><div id="date"></div><div id="description"></div>`;
+    Mock = jest.spyOn(global, 'fetch');
+    // all_user_entries = [{id:1, title:"fakeTitle", date_created:"30/08/2018", description:"this is a fakeentry"},{id:2, title:"fakeTitle", date_created:"30/08/2018", description:"this is another fakeentry"},{id:1, title:"fakeTitle", date_created:"30/08/2018", description:"this is another fakeentry"},{id:1, title:"fakeTitle", date_created:"30/08/2018", description:"this is another fakeentry"},{id:1, title:"fakeTitle", date_created:"30/08/2018", description:"this is another fakeentry"},{id:1, title:"fakeTitle", date_created:"30/08/2018", description:"this is another fakeentry"},{id:1, title:"fakeTitle", date_created:"30/08/2018", description:"this is another fakeentry"},{id:1, title:"fakeTitle", date_created:"30/08/2018", description:"this is another fakeentry"}];
+    // all_user_entries = [{id:1, title:"fakeTitle", date_created:"30/08/2018", description:"this is a fakeentry"}];
+    Mock = jest.spyOn(global, 'fetch');
+    welcome_message = {
+        "Welcome":"Hey! welcome to thriller diary api"
+        }
+    Mock.mockImplementation(() => Promise.resolve({
+    json: () => Promise.resolve({"status": "success", "Message": welcome_message})
+    }))
+    // spy on window navigation
+    NavSpyMock = jest.spyOn(window.location, "assign");
+    NavSpyMock.mockImplementation(() => {});
+  it("makes sure that homepage is loaded", async () => {
+
+    require('../app/static/js/welcome')
+    window.document.dispatchEvent(new Event("DOMContentLoaded", {
+        bubbles: true,
+        cancelable: true
+      }));
+        expect(Mock).toHaveBeenCalledTimes(1);
+        const Fetch = Mock.mock.calls[0];
+        expect(Fetch[0]).toBe('//api-thriller-diary.herokuapp.com/api/v1/auth/');
+        expect(Fetch[1]).toEqual({
+          method: "GET"
+        })
+        await Promise.resolve().then();
+        expect(document.getElementById('welcoming-message').innerHTML).toMatch("<p class=\"text-white\">");
+  });
+});
